340970118c3f321b86defaefd2249bbf0517ea8a
Shamaratun <shamaratunmeme@gmail.com>
2025-10-02 00:44:53 +0600

    discharge & followup  correction done

--- src/components/formValidationFinal/dischargeInfo.tsx
+++ src/components/formValidationFinal/dischargeInfo.tsx
@@ -3,14 +3,17 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import { TextField, TextAreaField, CheckboxField, RadioField, DateField } from "./reusableForm";
 import FollowupForm from "./followUpForm";
 
+
+
+
 const FormSchema = z.object({
-    discharge_date: z.string().min(1, "Discharge date is required"),
+    discharge_date: z.string().min(1, "Discharge date is required").refine((val) => !isNaN(Date.parse(val)), {
+      message: "Invalid date",
+    }),
     advice: z.string().min(1, "Advice is required"),
     fu_procedure: z.string().optional(),
     followup_required: z.boolean(),
@@ -45,7 +48,7 @@ export default function DischargeInfo() {
     };
 
     return (
-        <div className="flex flex-col md:flex-row gap-6 bg-gray-100 p-6 rounded-lg">
+        <div className="flex flex-col  md:flex-row gap-6 bg-gray-100 p-6 rounded-lg">
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-1/2 bg-gray-50 p-4 rounded shadow ml-0 mt-10">
                     <DateField control={form.control} name="discharge_date" label="Date of Discharge" />
